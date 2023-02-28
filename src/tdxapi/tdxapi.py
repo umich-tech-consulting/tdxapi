a608a8da9d6f5be33744939085545ba8f4122512
Owain Kert <owaink2255@gmail.com>
2023-02-28 16:48:26 -0500

    await response when populating specific app

--- src/tdxapi/tdxapi.py
+++ src/tdxapi/tdxapi.py
@@ -104,14 +104,14 @@ class TeamDynamixInstance:
         return
 
 
-    def populate_ids_for_app(self, app_type: str, app_name: str) -> None:
+    async def populate_ids_for_app(self, app_type: str, app_name: str) -> None:
         """Populates the TDx object with IDs for a specific app, like tickets or people
 
         Args:
             app_type (str): The type of the app, eg "AssetStatusIDs"
             app_name (str): The name of the app in TDx to populate, eg "ITS Tickets"
         """
-        asyncio.run(self._populate_ids(app_type, app_name))
+        await self._populate_ids(app_type, app_name)
         return
 
     def load_auth_token(self, filename: str = "tdx.key") -> None:
