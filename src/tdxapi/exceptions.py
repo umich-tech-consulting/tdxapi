321fbf8227e3b7557131578fca7200183824ef8c
Owain Kert <owaink2255@gmail.com>
2023-03-02 23:38:05 -0500

    style

--- src/tdxapi/exceptions.py
+++ src/tdxapi/exceptions.py
@@ -1,23 +1,33 @@
+"""TDxApi exceptions."""
+
+
 class ObjectNotFoundException(Exception):
-    """Object was not found in remote TDx instance"""
+    """Object was not found in remote TDx instance."""
+
 
 class AssetAttachFailedException(Exception):
-    """Attaching asset to ticket failed"""
+    """Attaching asset to ticket failed."""
+
 
 class NotAuthorizedException(Exception):
-    """Current user is not authorized in remote TDx instance"""
+    """Current user is not authorized in remote TDx instance."""
+
 
 class RequestFailedException(Exception):
-    """Generic error when sending request to remote TDx instance"""
+    """Generic error when sending request to remote TDx instance."""
+
 
 class NoDefaultAppException(Exception):
-    """No app was provided and no default is set"""
+    """No app was provided and no default is set."""
+
 
 class InvalidHTTPMethodException(Exception):
-    """Not a supported HTTP Method"""
+    """Not a supported HTTP Method."""
+
 
 class NoSuchAttributeException(Exception):
-    """The attribute could not be found in the object"""
+    """The attribute could not be found in the object."""
+
 
 class PropertyNotSetException(Exception):
-    """The property has not been set"""
+    """The property has not been set."""
